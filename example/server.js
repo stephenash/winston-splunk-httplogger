29cc814366f9e8b4a500b866cce88ab7da793cb5
Stephen Ash <stephenash@gmail.com>
2017-06-02 16:12:10 -0700

    eslint the entire codebase, not just main module.
    Also adds `npm run lint` command, and fixes up all errors.

--- example/server.js
+++ example/server.js
@@ -1,23 +1,23 @@
 /* eslint-env node */
 /* global process */
-var express = require('express'),
-    winston = require('winston'),
-    SplunkStreamEvent = require('winston-splunk-httplogger'),
-    expressLogger = require('express-winston');
+var express = require('express');
+var winston = require('winston');
+var SplunkStreamEvent = require('winston-splunk-httplogger');
+var expressLogger = require('express-winston');
 
 var app = express();
 
 var splunkSettings = {
-    host: process.env.SPLUNK_HOST || 'localhost',
-    token: process.env.SPLUNK_TOKEN
+  host: process.env.SPLUNK_HOST || 'localhost',
+  token: process.env.SPLUNK_TOKEN
 };
 
 app.use(expressLogger.logger({
-    transports: [
-        new winston.transports.Console(),
-        new SplunkStreamEvent({ splunk: splunkSettings })
-    ],
-    level: 'info'
+  transports: [
+    new winston.transports.Console(),
+    new SplunkStreamEvent({ splunk: splunkSettings })
+  ],
+  level: 'info'
 }));
 
 app.listen(process.env.PORT || 3000);
